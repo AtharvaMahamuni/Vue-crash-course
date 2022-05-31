36a18fb058d5feb474abe00c5d6a1f320f0ef70c
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-05-31 14:48:11 +0530

    some starting

---
+++ todo-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello from Atharva</h1>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+  import TodoList from './components/TodoList.vue';
+  export default {
+    name: 'App',
+    components: {
+    TodoList
+    }
+  }
+</script>
+
+<style>
+  body {
+    background: rgb(66,66,66);
+    color: aliceblue;
+  }
+</style>
+
---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+<h1>Hello from TODO list template</h1>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        title: 'Learn Vue',
+                        completed: true
+                    },
+                    {
+                        id: 2,
+                        title: 'Learn Vuex',
+                        completed: false
+                    },
+                    {
+                        id: 3,
+                        title: 'Learn Vue Router',
+                        completed: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            addTodo() {},
+            toggleTodo() {},
+            editTodo() {},
+            deleteTodo() {}
+        }
+    };
+</script>
+
+<style>
+</style>
